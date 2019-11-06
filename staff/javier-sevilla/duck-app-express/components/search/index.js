ddd6770488a45af72839ddb84484e3028f86c7b4
Javier Sevilla <xaviersevilla@gmail.com>
2019-11-06 18:11:40 +0100

    add express duck

---
+++ staff/javier-sevilla/duck-app-express/components/search/index.js
@@ -0,0 +1,71 @@
+module.exports = function() {
+    return `
+    <section class="view search">
+<h1 class="search__title">Search</h1>
+<h2 class="search__user">Pepito</h2><button class="search__logout">Logout</button>
+<form class="search__form">
+    <span class="search__icon">🐣</span>
+    <input class="search__criteria" type="text" name="query" placeholder="criteria">
+    <button class="search__submit">🔍</button>
+</form>
+
+<ul class="results">
+    <li class="results__item">
+        <a href="#" class="item">
+            <h2 class="item__title">This is a Super Duuuuuuuck</h2>
+            <img class="item__image" src="https://fakeimg.pl/300x200/?text=Hello">
+            <span class="item__price">10€</span>
+            <span class="item__fav">💔</span>
+        </a>
+    </li>
+    <li class="results__item">
+        <a href="#" class="item">
+            <h2 class="item__title">Duck X</h2>
+            <img class="item__image" src="https://fakeimg.pl/300x200/?text=Hello">
+            <span class="item__price">10€</span>
+            <span class="item__fav">🧡</span>
+        </a>
+    </li>
+    <li class="results__item">
+        <a href="#" class="item">
+            <h2 class="item__title">Duck X</h2>
+            <img class="item__image" src="https://fakeimg.pl/300x200/?text=Hello">
+            <span class="item__price">10€</span>
+            <span class="item__fav">💔</span>
+        </a>
+    </li>
+    <li class="results__item">
+        <a href="#" class="item">
+            <h2 class="item__title">Duck X</h2>
+            <img class="item__image" src="https://fakeimg.pl/300x200/?text=Hello">
+            <span class="item__price">10€</span>
+            <span class="item__fav">💔</span>
+        </a>
+    </li>
+    <li class="results__item">
+        <a href="#" class="item">
+            <h2 class="item__title">Duck X</h2>
+            <img class="item__image" src="https://fakeimg.pl/300x200/?text=Hello">
+            <span class="item__price">10€</span>
+            <span class="item__fav">💔</span>
+        </a>
+    </li>
+    <li class="results__item">
+        <a href="#" class="item">
+            <h2 class="item__title">Duck X</h2>
+            <img class="item__image" src="https://fakeimg.pl/300x200/?text=Hello">
+            <span class="item__price">10€</span>
+            <span class="item__fav">💔</span>
+        </a>
+    </li>
+    <li class="results__item">
+        <a href="#" class="item">
+            <h2 class="item__title">Duck X</h2>
+            <img class="item__image" src="https://fakeimg.pl/300x200/?text=Hello">
+            <span class="item__price">10€</span>
+            <span class="item__fav">💔</span>
+        </a>
+    </li>
+</ul>
+</section>`
+}
