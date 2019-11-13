4206bac9ac1c5dd77ba4624d6852e76d7765c7b0
Javier Sevilla <xaviersevilla@gmail.com>
2019-11-13 13:21:43 +0100

    add tasks first com

---
+++ staff/javier-sevilla/tasks/tasks-api/logic/register-user/index.js
@@ -0,0 +1,37 @@
+const validate = require('../../utils/validate')
+const users = require('../../data/users.json')
+const fs = require('fs')
+const path = require('path')
+
+module.exports = function(name, surname, email, username, password) {
+    validate.string(name)
+    validate.string.notVoid('name', name)
+    validate.string(surname)
+    validate.string.notVoid('surname', surname)
+    validate.string(email)
+    validate.string.notVoid('e-mail', email)
+    validate.string(username)
+    validate.string.notVoid('username', username)
+    validate.string(password)
+    validate.string.notVoid('password', password)
+
+    return new Promise((resolve, reject) => {
+        users.push({ name, surname, email, username, password })
+
+        // logic rules!!!!
+
+
+
+
+
+        
+        const json = fs.readFile(path.join(__dirname, '../../data/users.json'))
+        const usersDisk = JSON.parse(json)
+
+        const user = usersDisk.find(user => usersDisk.username === username)
+
+        if (user) return reject(new Error(error))
+
+        fs.writeFile(path.join(__dirname, '../../data/users.json'), JSON.stringify(users), error => error? reject(error) : resolve())
+    })
+}
