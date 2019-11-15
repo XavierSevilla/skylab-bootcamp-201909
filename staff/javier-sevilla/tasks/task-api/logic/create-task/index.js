a5a94150c7ac7d0d685ffaefda844ab08fd985dc
Javier Sevilla <xaviersevilla@gmail.com>
2019-11-15 09:47:01 +0100

    add create retrieve tasks

--- staff/javier-sevilla/tasks/task-api/logic/create-task/index.js
+++ staff/javier-sevilla/tasks/task-api/logic/create-task/index.js
@@ -1,30 +1,33 @@
 const validate = require('../../utils/validate')
 const users = require('../../data/users')()
+const tasks = require('../../data/tasks')()
 const uuid = require('uuid/v4')
-const { ConflictError } = require('../../utils/errors')
+const { NotFoundError } = require('../../utils/errors')
 
-module.exports = function (id, surname, email, username, password) {
-    validate.string(name)
-    validate.string.notVoid('name', name)
-    validate.string(surname)
-    validate.string.notVoid('surname', surname)
-    validate.string(email)
-    validate.string.notVoid('e-mail', email)
-    validate.email(email)
-    validate.string(username)
-    validate.string.notVoid('username', username)
-    validate.string(password)
-    validate.string.notVoid('password', password)
+module.exports = function (id, title, description) {
+    validate.string(id)
+    validate.string.notVoid('id', id)
+    validate.string(title)
+    validate.string.notVoid('title', title)
+    validate.string(description)
+    validate.string.notVoid('description', description)
 
     return new Promise((resolve, reject) => {
-        const user = users.data.find(user => user.username === username)
+        const user = users.data.find(user => user.id === id)
 
-        if (user) return reject(new ConflictError(`user with username ${username} already exists`))
+        if (!user) return reject(new NotFoundError(`user with id ${id} not found`))
 
-        const id = uuid()
+        const task = {
+            id: uuid(),
+            user: id,
+            title,
+            description,
+            status: 'TODO',
+            date: new Date
+        }
 
-        users.data.push({ id, name, surname, email, username, password })
+        tasks.data.push(task)
 
-        users.persist().then(resolve).catch(reject)
+        tasks.persist().then(() => resolve(task.id)).catch(reject)
     })
 }
--- staff/javier-sevilla/tasks/task-api/logic/index.js
+++ staff/javier-sevilla/tasks/task-api/logic/index.js
@@ -2,5 +2,6 @@ module.exports = {
     authenticateUser: require('./authenticate-user'),
     registerUser: require('./register-user'),
     retrieveUser: require('./retrieve-user'),
-    createTask: require('./create-task')
+    createTask: require('./create-task'),
+    retrieveTask : require('./retrieve-task')
 }
